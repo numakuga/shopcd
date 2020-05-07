a8e70098becabdf4ce14c593d337ca682ae07ffc
KUGA16 <gachan_1216@yahoo.co.jp>
2020-05-07 15:30:52 +0000

    Add: バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :first_name
+    validates :last_name
+    validates :first_kana
+    validates :last_kana
+    validates :postal_code, length: {is: 7}
+    validates :phone
+  end
+
 end
