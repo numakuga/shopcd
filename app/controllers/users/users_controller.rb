5bdddc47f2b58806e9c7c0e95943e97e411e84be
KUGA16 <gachan_1216@yahoo.co.jp>
2020-05-07 15:51:08 +0000

    Add: プロフィール編集機能実装したよー

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,17 +1,35 @@
 class Users::UsersController < ApplicationController
+
+  before_action :user_find
+
   def show
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def exit
   end
 
   def update
+    if @user.update(user_params)
+      flash[:notice] = "プロフィールを編集しました。"
+      redirect_to user_path(current_user)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def user_find
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+      params.require(:user).permit(:first_name, :last_name, :last_name, :last_kana, :postal_code, :address, :phone, :email)
   end
 end
