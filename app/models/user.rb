6339e43c79147ecb1884cc21a7cde4aa83ef9711
KUGA16 <gachan_1216@yahoo.co.jp>
2020-05-10 05:16:45 +0000

    Add: リレーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :addresses, dependent: :destroy
+  has_many :addresses,  dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :items,      through: :cart_items, dependent: :destroy
+  has_many :orders,     dependent: :destroy
 
   with_options presence: true do
     validates :first_name
