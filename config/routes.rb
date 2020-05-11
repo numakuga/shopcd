e2f201991c83e5a3a33b3b59a69d39da467c1654
KUGA16 <gachan_1216@yahoo.co.jp>
2020-05-11 11:54:51 +0000

    Fix: rootをscope内に移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,30 @@
 Rails.application.routes.draw do
   # new index show edit create update destroy
-  root 'items#index'
 
   devise_for :admins
   devise_for :users
 
   scope module: :users do
+
+    root 'items#index'
+
     # users
     resources :users, except:[:new,:index,:create] do
+      # cart_items
       resources :cart_items, only:[:index,:create,:update,:destroy]
     end
     get 'users/:id/exit' => 'users#exit', as: :exit
+
     # addresses
     resources :addresses, only:[:create,:update,:destroy]
-    # cart_items
 
     # orders
     resources :orders, only:[:index,:new,:create]
     get 'orders/thanks'
+
     # items
     resources :items, only:[:show]
+    
     # searches
     get 'searches/items' => 'searches#items'
     get 'searches/artists' => 'searches#artists'
