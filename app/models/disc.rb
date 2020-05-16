d7a351a790eabe613b6f4d58ada861cb00a8e2f7
KUGA16 <gachan_1216@yahoo.co.jp>
2020-05-16 13:51:18 +0000

    Add: リレーション追加

--- app/models/disc.rb
+++ app/models/disc.rb
@@ -1,3 +1,4 @@
 class Disc < ApplicationRecord
   belongs_to :item
+  has_many   :songs, dependent: :destroy
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :artist
   belongs_to :label
   belongs_to :genre
+  has_many   :discs, dependent: :destroy
 
   # カートに入れるアイテム個数選択用の配列
   def stock_array
