4b0133d1a03e5300e52c14cc30193f0d63530297
KUGA16 <gachan_1216@yahoo.co.jp>
2020-05-21 14:57:32 +0000

    Add: ログインユーザーのカートアイテムを取得

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -2,6 +2,7 @@ class Users::OrdersController < ApplicationController
   def index
     @order = Order.new
     @addresses = Address.where(user_id: current_user.id)
+    @cart_items = CartItem.where(user_id: current_user.id)
   end
 
   def new
