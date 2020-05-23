f2771a2196ca3f3fbd622977ef3deaa4959f1fcf
KUGA16 <gachan_1216@yahoo.co.jp>
2020-05-23 14:03:34 +0000

    Add: refileで画像を挿入できるように変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,14 @@
 class Item < ApplicationRecord
+  # リレーション
   belongs_to :artist
   belongs_to :label
   belongs_to :genre
   has_many   :cart_items
   has_many   :users, through: :cart_items
   has_many   :discs, dependent: :destroy
+
+  # refile
+  attachment :jacket_image
 
   # カートに入れるアイテム個数選択用の配列
   def stock_array
