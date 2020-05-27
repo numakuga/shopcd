375d89acc1af292a7077e13cc1f6895396d410d7
saki918 <hiro9181013@gmail.com>
2020-05-27 17:32:38 +0900

    refile 画像保存の設定記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
   belongs_to :label
   belongs_to :genre
 
+  attachment :jacket_image
+
   # カートに入れるアイテム個数選択用の配列
   def stock_array
     self.stock == 0 ? ["売り切れ"]:[*1..self.stock]
