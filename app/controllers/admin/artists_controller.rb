c7e6db20823fad0cfc2a43b478a90621cdb53850
saki918 <hiro9181013@gmail.com>
2020-05-27 17:31:39 +0900

    コントローラ index edit create update destroy 作成

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -1,25 +1,46 @@
-class Admin::ArtistsController < ApplicationController
+class Admin::ArtistsController < Admin::AdminapplicationsController
   def index
     @new_artist = Artist.new
+    @artists = Artist.all
+  end
+
+  def edit
+    @edit_model = Artist.find(params[:id])
+    @model_name = "アーティスト"
+    render 'admin/shared/edit'
   end
 
   def create
-    new_artist = Artist.new(artist_params)
-    if new_artist.save
+    @new_artist = Artist.new(artist_params)
+    if @new_artist.save
       flash[:notice] = "アーティストを作成しました"
       redirect_to admin_artists_path
     else
+      # @new_artist = Artist.new
+      @artists = Artist.all
       render "index"
     end
   end
 
   def update
+    @edit_artist = Artist.find(params[:id])
+    if @edit_artist.update(artist_params)
+      flash[:notice] = "アーティストを編集しました！"
+      redirect_to admin_artists_path
+    else
+      render "admin/shared/edit"
+    end
   end
 
   def destroy
+    artist = Artist.find(params[:id])
+    artist.destroy
+    flash[:notice] = "アーティストを削除しました"
+    # redirect_to のときはpath名を指定
+    redirect_to admin_artists_path
   end
 
-   private
+  private
 
   def artist_params
     params.require(:artist).permit(:name)
--- app/controllers/admin/labels_controller.rb
+++ app/controllers/admin/labels_controller.rb
@@ -1,24 +1,45 @@
-class Admin::LabelsController < ApplicationController
+class Admin::LabelsController < Admin::AdminapplicationsController
+  before_action :current_admin
+
   def index
     @new_label = Label.new
     @labels = Label.all
   end
 
+  def edit
+    @edit_model = Label.find(params[:id])
+    @model_name = "レーベル"
+    render 'admin/shared/edit'
+  end
+  
   def create
-    # binding.pry
-    new_label = Label.new(label_params)
-    if new_label.save
+    @new_label = Label.new(label_params)
+    if @new_label.save
       flash[:notice] = "レーベルを作成しました"
       redirect_to admin_labels_path
     else
-      render "index"
+      @labels = Label.all
+      render 'index'
+      # binding.pry
     end
   end
 
   def update
+    @edit_model = Label.find(params[:id])
+    if @edit_model.update(label_params)
+      flash[:notice] = "レーベルを編集しました！"
+      redirect_to admin_labels_path
+    else
+      render ??
+    end
   end
 
   def destroy
+    label = Label.find(params[:id])
+    label.destroy
+    flash[:notice] = "レーベルを削除しました"
+    # redirect_to のときはpath名を指定
+    redirect_to admin_labels_path
   end
 
   private
