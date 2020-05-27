4742e0e667ddcb9c3efe95c25a6635d3a16be290
saki918 <hiro9181013@gmail.com>
2020-05-27 17:39:15 +0900

    admin itemコントローラ index show new create update 作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,23 +1,49 @@
-class Admin::ItemsController < ApplicationController
+class Admin::ItemsController < Admin::AdminapplicationsController
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
     @new_item = Item.new
+    @artists = Artist.all
+    @labels = Label.all
+    @genres = Genre.all
   end
 
   def edit
   end
 
   def create
+    @new_item = Item.new(item_params)
+    if @new_item.save
+      flash[:notice] = "商品情報を作成しました"
+      redirect_to admin_item_path(@new_item)
+    else
+      render "new"
+    end
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice] = "アーティストを編集しました！"
+      redirect_to admin_item_path
+    else
+      render "admin/shared/edit"
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :price, :status, :jacket_image, :stock, :release, :artist_id, :label_id, :genre_id)
+  end
+
 end
