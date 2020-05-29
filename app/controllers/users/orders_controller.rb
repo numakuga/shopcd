2a70c9be1fc877415c36bb004e27f0be591d28bc
KUGA16 <gachan_1216@yahoo.co.jp>
2020-05-29 07:14:16 +0000

    Add: とりあえず表示は完了（これからcreate）

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -10,7 +10,10 @@ class Users::OrdersController < ApplicationController
   end
 
   def confirm
-    @payment = params[:payment]
+    flash.now[:notice] = "まだ購入は完了していません。"
+    @delivery_cost = 500
+    @order = current_user.orders.new(order_params)
+    
   end
 
   def create
@@ -18,5 +21,34 @@ class Users::OrdersController < ApplicationController
   end
 
   def thanks
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
+  end
+
+  def order_params
+    case params[:address_num].to_i
+    when 0 #ログインユーザー登録住所
+      # @full_name = current_user.first_name + current_user.last_name
+      postal_code = current_user.postal_code
+      address = current_user.address
+    when 1 #フォームで入力住所
+      address = params[:address]
+      postal_code = params[:postal_code]
+    when 2 #セレクトタグで選択住所
+      address_record = Address.params[:address_id]
+      postal_code = address_record.postal_code
+      address = address_record.address
+    end
+    {
+      postal_code: postal_code,
+      address: address,
+      payment: params[:payment],
+      total_price: current_user.cart_total_price + @delivery_cost,
+      delivery_cost: @delivery_cost
+    }
   end
 end
