fa1d1ea0e05b85037c1dad0def980238cfd63867
KUGA16 <gachan_1216@yahoo.co.jp>
2020-05-29 06:41:10 +0000

    Add: カート内の商品数と合計を表示するメソッド作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,21 @@ class User < ApplicationRecord
   def kana_full_name
     "#{first_kana} #{last_kana}"
   end
+
+  def cart_total_price
+    total_price = 0
+    cart_items.each do |cart_item|
+      # round = 小数点以下切り捨て
+      total_price += (cart_item.piece * cart_item.item.price * 1.10).round
+    end
+    return total_price
+  end
+
+  def cart_total_piece
+    total_piece = 0
+    cart_items.each do |cart_item|
+      total_piece += cart_item.piece
+    end
+    return total_piece
+  end
 end
