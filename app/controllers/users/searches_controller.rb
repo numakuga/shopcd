60a6671f287ed7813c81ead6519307381932a8c1
KUGA16 <gachan_1216@yahoo.co.jp>
2020-06-05 08:43:10 +0000

    Add: 検索でDBになかった商品は楽天APIで取得できる様に実装、まだ細かい条件分岐はできていないのでエラー出ちゃうかも、、、

--- app/controllers/users/searches_controller.rb
+++ app/controllers/users/searches_controller.rb
@@ -1,9 +1,54 @@
 class Users::SearchesController < ApplicationController
-
   def items
     @q = Item.ransack(ransack_item_params)
     @items = @q.result
-    render "users/items/index"
+    # DBにない場合にAPIを叩く&APIの情報を保存する
+    if @items == []
+      rakuten_items = RakutenWebService::Books::CD.search(title: ransack_item_params.values)
+      rakuten_items.each do |rakuten_item|
+        # DBに同じアーティスト・レーベルがいた場合
+        artist = Artist.find_by(name: rakuten_item.artist_name)
+        label = Label.find_by(name: rakuten_item.label)
+        unless artist.present?
+          # DBに同じアーティストがいない場合、新しくレコードを作成・保存
+          artist = Artist.create(name: rakuten_item.artist_name)
+        end
+        unless label.present?
+          # DBに同じレーベルがいない場合、新しくレコードを作成・保存
+          label = Label.create(name: rakuten_item.label)
+        end
+        #genreIDはまた別のAPIを叩きに行き、genreモデルに保存
+        # ここでたまにid長いって怒られる
+        genre_params = RakutenWebService::Books::Genre.search(books_genre_id: rakuten_item.books_genre_id)
+        genre_params.each do |g|
+          genre = Genre.create(name: g.parents[0]["books_genre_name"])
+          # itemのDBに保存したい
+          item = Item.new(
+            artist_id: artist.id,
+            label_id: label.id,
+            genre_id: genre.id,
+            title: rakuten_item.title,
+            price: rakuten_item.item_price, #税込み販売価格
+            jacket_image_id: rakuten_item.large_image_url,
+            release: Date.strptime(rakuten_item.sales_date,'%Y年%m月%d日'), # 文字列からdate型に変換
+            stock: 30
+          )
+          # itemが保存できたら、紐ついているdisc、songもcreate
+          if item.save
+            disc = Disc.create(item_id: item.id)
+            # itemないの曲をeach?
+            rakuten_item.play_list.split("###").each do |song|
+              Song.create(disc_id: disc.id, title: song)
+            end
+          end
+        end
+      end
+      @q = Item.ransack(ransack_item_params)
+      @items = @q.result
+      render "users/items/index"
+    else
+      render "users/items/index"
+    end
   end
 
   private
